765402d2ac12780b727c99daa723bffee6ac455e
god <evgeniy246806@gmail.com>
2021-10-05 05:46:59 +0300

    Simple android chat based at firebase

---
+++ app/src/main/java/com/example/cloudchat/MainActivity.java
@@ -0,0 +1,109 @@
+package com.example.cloudchat;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int MAX_MESSAGE_LENGTH = 512;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("messages");
+
+    EditText mEditTextMessage;
+    Button mSendButton;
+    RecyclerView mMessagesRecycler;
+
+    ArrayList<String> messages = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSendButton = findViewById(R.id.search_message_b);
+        mEditTextMessage = findViewById(R.id.message_input);
+        mMessagesRecycler = findViewById(R.id.messages_recycler);
+
+        mMessagesRecycler.setLayoutManager(new LinearLayoutManager(this));
+
+        DataAdapter dataAdapter = new DataAdapter(this,  messages);
+
+        mMessagesRecycler.setAdapter(dataAdapter);
+
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                String msg = mEditTextMessage.getText().toString();
+
+                if (msg.equals("")) {
+                    Toast.makeText(getApplicationContext(), "Enter message!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (msg.length() >= MAX_MESSAGE_LENGTH) {
+                    Toast.makeText(getApplicationContext(), "Message too long!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                myRef.push().setValue(msg);
+                mEditTextMessage.setText("");
+            }
+        });
+
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                String msg = snapshot.getValue(String.class);
+
+                messages.add(msg);
+                dataAdapter.notifyDataSetChanged();
+                mMessagesRecycler.smoothScrollToPosition(messages.size());
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/cloudchat/ViewHolder.java
@@ -0,0 +1,18 @@
+package com.example.cloudchat;
+
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ViewHolder extends RecyclerView.ViewHolder {
+
+    TextView message;
+
+    public ViewHolder(View itemView) {
+
+        super(itemView);
+        message = itemView.findViewById(R.id.message_item);
+    }
+}
